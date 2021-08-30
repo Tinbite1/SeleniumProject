29316c42d09d8ebeda9545583db94246898f14d6
Akbar <akbar@cybertekschool.com>
2021-08-30 17:00:57 -0400

    Day01 : FindElement By Name practice Find element by link text completed

--- src/test/java/com/cybertek/day01_navigation_locators/FindElementByLinkText.java
+++ src/test/java/com/cybertek/day01_navigation_locators/FindElementByLinkText.java
@@ -1,29 +1,38 @@
 package com.cybertek.day01_navigation_locators;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FindElementByLinkText {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
         // set up chrome driver , open chrome
-
-
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
         // navigate to https://google.com
-
+        driver.get("https://google.com");
         // try to identify search box using both name and id
-
-        // submit the search by clicking on the search button
-
+        WebElement searchBox = driver.findElement(By.name("q")) ;
+                   searchBox.sendKeys("Everything is awesome");
+;         // submit the search by clicking on the search button
+        WebElement searchBtn = driver.findElement(By.name("btnK")) ;
+                    searchBtn.submit();
         // navigate back to google home page
-
+        driver.navigate().back();;
         // wait few second
-
+        Thread.sleep(3000);
         // click on about link
-
-
+        WebElement aboutLink = driver.findElement(By.linkText("About") ) ;
+        aboutLink.click();
         // wait few seconds
-
+        Thread.sleep(3000);
         // close the browser
+        driver.quit();
 
 
 
