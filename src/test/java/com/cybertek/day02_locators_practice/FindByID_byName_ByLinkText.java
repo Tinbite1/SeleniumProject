974d718022e55ddc8ea16ff550625da48896ac87
Akbar <akbar@cybertekschool.com>
2021-09-02 11:17:14 -0400

    Day 02 : Recap navigation and findElement by linktext

--- src/test/java/com/cybertek/day02_locators_practice/FindByID_byName_ByLinkText.java
+++ src/test/java/com/cybertek/day02_locators_practice/FindByID_byName_ByLinkText.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindByID_byName_ByLinkText {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -26,6 +26,10 @@ public class FindByID_byName_ByLinkText {
         WebElement loginBtn = driver.findElement(By.name("login")) ;
         loginBtn.click();
 
+        Thread.sleep(5000);
+
+        WebElement forgetLink = driver.findElement(By.linkText("Forgot Password?")) ;
+        forgetLink.click();
 
         // quit the browser
         driver.quit();
