48a1688ecb935bf1218d448b1ebe96133d5627e9
Akbar <akbar@cybertekschool.com>
2021-09-02 12:22:52 -0400

    Day 02 : Partial Link Text to identify then getText method to get full text

---
+++ src/test/java/com/cybertek/day02_locators_practice/FindByPartialText_GetText_Method.java
@@ -0,0 +1,30 @@
+package com.cybertek.day02_locators_practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindByPartialText_GetText_Method {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/");
+
+        // identify the link with text "Disappearing Elements"
+        // using partial link text then get the full text out using new method getText
+        WebElement myLnk = driver.findElement(By.partialLinkText("Disappearing")) ;
+
+        //System.out.println("myLnk.getText() = " + myLnk.getText() );
+
+        String fullLinkText = myLnk.getText() ;
+        System.out.println("fullLinkText = " + fullLinkText);
+
+
+    }
+
+}
