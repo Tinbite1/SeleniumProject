134304c18a6cf3be6989e2c53affd1de562b766d
Akbar <akbar@cybertekschool.com>
2021-09-02 11:25:47 -0400

    Day 02 : LinkText Practice starter

---
+++ src/test/java/com/cybertek/day02_locators_practice/LinkTextPartialLinkTextPractice.java
@@ -0,0 +1,20 @@
+package com.cybertek.day02_locators_practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LinkTextPartialLinkTextPractice {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/");
+
+
+
+    }
+
+}
