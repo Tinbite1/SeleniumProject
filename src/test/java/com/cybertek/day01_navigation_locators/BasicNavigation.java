02e05abf792ea781d2047ccd2021f118da5bb2c9
Tinbite1 <80796613+Tinbite1@users.noreply.github.com>
2023-02-08 10:39:31 -0500

    added s back

--- src/test/java/com/cybertek/day01_navigation_locators/BasicNavigation.java
+++ src/test/java/com/cybertek/day01_navigation_locators/BasicNavigation.java
@@ -8,7 +8,7 @@ public class BasicNavigation {
 
     public static void main(String[] args) {
 
-        // set up chrome drive
+        // set up chrome drives
         WebDriverManager.chromedriver().setup();
         // create ChromeDriver Object
         WebDriver driver = new ChromeDriver();
